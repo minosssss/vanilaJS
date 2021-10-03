e32b82cfda5788ead92adbb77d58f0e3f5007344
mh.lee <mh.lee239@gmail.com>
2021-10-03 16:17:16 +0900

    if,else event

--- app.js
+++ app.js
@@ -3,30 +3,15 @@ const h1 = document.querySelector("div.minO h1");
 console.dir(h1);
 
 function handleTitleClick(){
-    console.log("h1 was clicked!");
-    h1.style.color = 'blue';
+    const currentColor = h1.style.color;
+    let newColor
+    if(currentColor == 'blue'){
+        newColor = 'tomato';
+    } else {
+        newColor = 'blue';
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
 h1.addEventListener("click", handleTitleClick);
 
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-//offline , online 
