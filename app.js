929154421bfbb7adc8c9fd9883070741f1910bbf
mh.lee <mh.lee239@gmail.com>
2021-10-07 23:56:45 +0900

    Learn JS Event,Arg

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const h1 = document.querySelector("div.minO h1");
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function handleTitleClick() {
-    //toggle은 class에 해당 이름이 존재하면 없애주고, 존재하지 않는다면 추가해줌 
-    h1.classList.toggle('clicked');
+function onLoginSubmit(event) {
+//form에서 발생한 submit 'event' 정보를 담을 용도로 
+//onLoginSubmit 함수에 event라는 매개변수를 추가
+    event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기
+    console.log(event);
 }
-
-h1.addEventListener("click", handleTitleClick);
-
+// addEventListener는 function의 이름만 적어주면 ()없이도 브라우저가 자동으로 실행한다.
+loginForm.addEventListener("submit",onLoginSubmit)
