de5e882bee578d0007a9043acd621cf5dffbe153
mh.lee <mh.lee239@gmail.com>
2021-10-08 00:25:16 +0900

    Learn JS Event & Getting name & classList

--- app.js
+++ app.js
@@ -3,12 +3,28 @@
 // const loginButton = loginForm.querySelector("button");
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"; // 중요한 정보를 담은 것이 아니라 대문자로 작성
+// const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
 //form에서 발생한 submit 'event' 정보를 담을 용도로 
 //onLoginSubmit 함수에 event라는 매개변수를 추가
     event.preventDefault(); // 브라우저가 기본 동작을 실행하지 못하게 막기
-    console.log(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // classList는 선택된 Element에 해당 class를 넣어줌
+    const username = loginInput.value;
+    // 아래는 새로운 방식이며, 백틱(`)을 사용하여 ${} 대괄호 안에 넣음
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+// function handleLinkClick(event) {
+//     event.preventDefault();
+//     console.dir(event);    
+// }
+
 // addEventListener는 function의 이름만 적어주면 ()없이도 브라우저가 자동으로 실행한다.
 loginForm.addEventListener("submit",onLoginSubmit)
+// link.addEventListener("click", handleLinkClick)
